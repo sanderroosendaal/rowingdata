05242215c25bb977fdd276f71a4ceaef9ab4015e
Sander Roosendaal <roosendaalsander@gmail.com>
2016-11-18 18:19:20 +0100

    parser

--- rowingdata/rowingdata.py
+++ rowingdata/rowingdata.py
@@ -63,7 +63,7 @@ from scipy import interpolate
 from scipy.interpolate import griddata
 
 
-__version__ = "0.92.6"
+__version__ = "0.92.7"
 
 namespace = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
 
