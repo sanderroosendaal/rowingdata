a5e252be3db2d8756ceb642e0e4fe39eecd14e7d
Sander Roosendaal <roosendaalsander@gmail.com>
2016-11-29 10:18:17 +0100

    Speedcoach

--- rowingdata/rowingdata.py
+++ rowingdata/rowingdata.py
@@ -64,7 +64,7 @@ from scipy import interpolate
 from scipy.interpolate import griddata
 
 
-__version__ = "0.92.9"
+__version__ = "0.93.0"
 
 namespace = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
 
