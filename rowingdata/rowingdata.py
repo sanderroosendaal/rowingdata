38a1f81f0ffdb0760faf9d26d5f55ecc94a46673
Sander Roosendaal <roosendaalsander@gmail.com>
2017-01-09 21:41:55 +0100

    bumped version nr

--- rowingdata/rowingdata.py
+++ rowingdata/rowingdata.py
@@ -8,7 +8,7 @@ import checkdatafiles
 
 #warnings.warn("Experimental version. Downgrade to 0.93.6 if you are not adventurous.",UserWarning)
 
-__version__ = "0.95.2"
+__version__ = "0.95.3"
 
 try:
     from Tkinter import Tk
