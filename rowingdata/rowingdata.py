35963b6f90d92bb8e70bf9c7e3959566a2173de4
Sander Roosendaal <roosendaalsander@gmail.com>
2017-01-11 13:04:59 +0100

    TCX bug fix

--- rowingdata/rowingdata.py
+++ rowingdata/rowingdata.py
@@ -8,7 +8,7 @@ import checkdatafiles
 
 #warnings.warn("Experimental version. Downgrade to 0.93.6 if you are not adventurous.",UserWarning)
 
-__version__ = "0.95.3"
+__version__ = "0.95.4"
 
 try:
     from Tkinter import Tk
