eabdd82250ae806d7524ca11f3776f0e99bc0f61
Sander Roosendaal <roosendaalsander@gmail.com>
2017-04-03 11:46:19 +0200

    some improvements in setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,14 @@ import re
 
 def readme():
     with open('README.rst') as f:
-	return f.read()
+        return f.read()
 
 setup(name='rowingdata',
-
       version=re.search(
-
-	  '^__version__\s*=\s*"(.*)"',
+          '^__version__\s*=\s*"(.*)"',
 	  open('rowingdata/rowingdata.py').read(),
 	  re.M
-
-	  ).group(1),
+      ).group(1),
 
       description='The rowingdata library to create colorful plots from CrewNerd, Painsled and other rowing data tools',
 
@@ -29,9 +26,9 @@ setup(name='rowingdata',
       license='MIT',
 
       # packages=['rowingdata'],
-      packages = find_packages(),
+      packages=find_packages(),
 
-      keywords = 'rowing ergometer concept2',
+      keywords='rowing ergometer concept2',
       
       install_requires=[
 	  'numpy',
@@ -47,7 +44,7 @@ setup(name='rowingdata',
 	  'iso8601',
           'lxml',
           'nose_parameterized',
-	  ],
+      ],
 
       zip_safe=False,
       include_package_data=True,
@@ -61,12 +58,12 @@ setup(name='rowingdata',
 	      'RP_testdata.csv',
               'SpeedCoach2example.csv',
 	      'testdata.csv'
-	      ],
-	  'bin':[
+	  ],
+          'bin':[
 	      'testdata.csv',
 	      'crewnerddata.csv',
 	      'crewnerddata.tcx',
-	      ],
+	  ],
 	  'rigging':[
 	      '1x.txt',
 	      '2x.txt',
@@ -75,45 +72,44 @@ setup(name='rowingdata',
 	      '4+.txt',
 	      '2-.txt',
 	      '8+.txt'
-	      ]
-	  },
-
-      entry_points = {
+	  ]
+      },
+      
+      entry_points={
 	  "console_scripts": [
-	      'rowingdata = rowingdata.rowingdata:main',
-	      'painsledtoc2 = rowingdata.painsledtoc2:main',
-	      'painsledplot = rowingdata.painsledplot:main',
-	      'crewnerdplot = rowingdata.crewnerdplot:main',
-	      'tcxtoc2 = rowingdata.tcxtoc2:main',
-#	      'painsled_desktop_plot = rowingdata.painsled_desktop_plot:main',
-	      'painsled_desktop_toc2 = rowingdata.painsled_desktop_toc2:main',
-	      'painsledplottime = rowingdata.painsledplottime:main',
-	      'painsled_desktop_plottime = rowingdata.painsled_desktop_plottime:main',
-	      'crewnerdplottime = rowingdata.crewnerdplottime:main',
-	      'roweredit = rowingdata.roweredit:main',
-	      'copystats = rowingdata.copystats:main',
-	      'tcxplot = rowingdata.tcxplot:main',
-	      'tcxplottime = rowingdata.tcxplottime:main',
-	      'tcxplot_nogeo = rowingdata.tcxplot_nogeo:main',
-	      'tcxplottime_nogeo = rowingdata.tcxplottime_nogeo:main',
-	      'speedcoachplot = rowingdata.speedcoachplot:main',
-	      'speedcoachplottime = rowingdata.speedcoachplottime:main',
-	      'speedcoachtoc2 = rowingdata.speedcoachtoc2:main',
-	      'rowproplot = rowingdata.rowproplot:main',
-	      'ergdataplot = rowingdata.ergdataplot:main',
-	      'ergdataplottime = rowingdata.ergdataplottime:main',
-	      'ergdatatotcx = rowingdata.ergdatatotcx:main',
-	      'ergstickplot = rowingdata.ergstickplot:main',
-	      'ergstickplottime = rowingdata.ergstickplottime:main',
-	      'ergsticktotcx = rowingdata.ergsticktotcx:main',
-	      'windcorrected = rowingdata.windcorrected:main',
-	      'boatedit = rowingdata.boatedit:main',
-	      'rowproplottime = rowingdata.rowproplottime:main'
-	      ]
-	  },
-
+	      'rowingdata=rowingdata.rowingdata:main',
+	      'painsledtoc2=rowingdata.painsledtoc2:main',
+	      'painsledplot=rowingdata.painsledplot:main',
+	      'crewnerdplot=rowingdata.crewnerdplot:main',
+	      'tcxtoc2=rowingdata.tcxtoc2:main',
+	      'painsled_desktop_toc2=rowingdata.painsled_desktop_toc2:main',
+	      'painsledplottime=rowingdata.painsledplottime:main',
+	      'painsled_desktop_plottime=rowingdata.painsled_desktop_plottime:main',
+	      'crewnerdplottime=rowingdata.crewnerdplottime:main',
+	      'roweredit=rowingdata.roweredit:main',
+	      'copystats=rowingdata.copystats:main',
+	      'tcxplot=rowingdata.tcxplot:main',
+	      'tcxplottime=rowingdata.tcxplottime:main',
+	      'tcxplot_nogeo=rowingdata.tcxplot_nogeo:main',
+	      'tcxplottime_nogeo=rowingdata.tcxplottime_nogeo:main',
+	      'speedcoachplot=rowingdata.speedcoachplot:main',
+	      'speedcoachplottime=rowingdata.speedcoachplottime:main',
+	      'speedcoachtoc2=rowingdata.speedcoachtoc2:main',
+	      'rowproplot=rowingdata.rowproplot:main',
+	      'ergdataplot=rowingdata.ergdataplot:main',
+	      'ergdataplottime=rowingdata.ergdataplottime:main',
+	      'ergdatatotcx=rowingdata.ergdatatotcx:main',
+	      'ergstickplot=rowingdata.ergstickplot:main',
+	      'ergstickplottime=rowingdata.ergstickplottime:main',
+	      'ergsticktotcx=rowingdata.ergsticktotcx:main',
+	      'windcorrected=rowingdata.windcorrected:main',
+	      'boatedit=rowingdata.boatedit:main',
+	      'rowproplottime=rowingdata.rowproplottime:main'
+	  ]
+      },
+      
       scripts=[
 	  'bin/painsledplot2.py',
-	  ]
-
-      )
+      ]
+      
+)
