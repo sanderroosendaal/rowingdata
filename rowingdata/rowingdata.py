e4eda2666305a9a5cf0864b20d15e10523319c65
Sander Roosendaal <roosendaalsander@gmail.com>
2019-02-07 09:13:32 +0100

    travis change

--- rowingdata/rowingdata.py
+++ rowingdata/rowingdata.py
@@ -3214,9 +3214,13 @@ class rowingdata:
 
 
         # modify pace/spm/wind with rolling averages
-        ps = df[' Stroke500mPace (sec/500m)'].rolling(skiprows+1).mean()
-        spms = df[' Cadence (stokes/min)'].rolling(skiprows+1).mean()
-
+        try:
+            ps = df[' Stroke500mPace (sec/500m)'].rolling(skiprows+1).mean()
+            spms = df[' Cadence (stokes/min)'].rolling(skiprows+1).mean()
+        except AttributeError:
+            ps = df[' Stroke500mPace (sec/500m)'].mean()
+            spms = df[' Cadence (stokes/min)'].mean()
+            
         if storetable is not None:
             try:
                 if storetable[-3:] != 'npz':
